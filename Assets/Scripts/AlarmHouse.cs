e7ef3323251c7762eb0229fd9d1231ec0f3fc60f
nosochken <kookier.armadas-0k@icloud.com>
2024-06-20 12:45:06 +0300

    fix

--- Assets/Scripts/AlarmHouse.cs
+++ Assets/Scripts/AlarmHouse.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(CompositeCollider2D), typeof(Rigidbody2D))]
 public class AlarmHouse : MonoBehaviour
 {
-    [SerializeField] private Witness _witness;
-
     private CompositeCollider2D _collider;
     private Rigidbody2D _rigidbody;
-    private Bounds _bounds;
 
     private Crook _crook;
-
-    private bool _isCrookOnriggerEnter;
-    private bool _isCrookOnriggerExit;
     private bool _isCrookInside;
 
     public event Action CrookCameIn;
     public event Action CrookCameOut;
-
-    public Bounds LocationBounds => _bounds;
 
     public bool IsCrookInside => _isCrookInside;
 
@@ -27,20 +19,10 @@ public class AlarmHouse : MonoBehaviour
     {
         _collider = GetComponent<CompositeCollider2D>();
         _collider.isTrigger = true;
-        _bounds = _collider.bounds;
+        _collider.geometryType = CompositeCollider2D.GeometryType.Polygons;
 
         _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbody.bodyType = RigidbodyType2D.Static;
-    }
-
-    private void OnEnable()
-    {
-        _witness.CrookSpottedInHouse += DetectCrook;
-    }
-
-    private void OnDisable()
-    {
-        _witness.CrookSpottedInHouse -= DetectCrook;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -48,7 +30,9 @@ public class AlarmHouse : MonoBehaviour
         if (collider.TryGetComponent(out Crook crook))
         {
             _crook = crook;
-            _isCrookOnriggerEnter = true;
+
+            if (_isCrookInside == false)
+                DetectCrook();
         }
     }
 
@@ -57,21 +41,8 @@ public class AlarmHouse : MonoBehaviour
         if (collider.TryGetComponent(out Crook crook))
         {
             if (_crook == crook)
-                _isCrookOnriggerExit = true;
+                LoseSightOfCrook();
         }
-
-        if (_isCrookOnriggerEnter && _isCrookOnriggerExit == true)
-            RespondToActionOfCrook();
-
-        ResetTriggerCrossingData();
-    }
-
-    private void RespondToActionOfCrook()
-    {
-        if (_isCrookInside == false)
-            DetectCrook();
-        else
-            LoseSightOfCrook();
     }
 
     private void DetectCrook()
@@ -84,11 +55,5 @@ public class AlarmHouse : MonoBehaviour
     {
         _isCrookInside = false;
         CrookCameOut?.Invoke();
-    }
-
-    private void ResetTriggerCrossingData()
-    {
-        _isCrookOnriggerEnter = false;
-        _isCrookOnriggerExit = false;
     }
 }
--- Assets/Scripts/Crook.cs
+++ Assets/Scripts/Crook.cs
@@ -1,17 +1,4 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
-public class Crook : MonoBehaviour
-{
-    private Collider2D _collider;
-    private Bounds _bounds;
-
-    public Bounds LocationBounds => _bounds;
-
-    private void Awake()
-    {
-        _collider = GetComponent<Collider2D>();
-
-        _bounds = _collider.bounds;
-    }
-}
+public class Crook : MonoBehaviour { }
